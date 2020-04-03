9c9bde3bef6e6fb40372d3ebc8ad953ba2728ae4
belladewusa <55181780+belladewusa@users.noreply.github.com>
2020-04-03 17:28:27 +0700

    Update soal3.c

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -5,22 +5,23 @@
 #include<string.h>
 #include<pthread.h>
 #include<stdlib.h>
-#include<unistd.h>
+#include<unistd.h> 
+
 #define MAX 256
 
-pthread_t tid[2000];
+  pthread_t tid[2000]; 
 
-char namafile[30];
-char namafilebaru[30];
-char ekstensi[10];
-char dirawal[20];
-char dirakhir[20];
-char f[] = "-f";
-char bintang = "*";
-char d[] = "-d";
-void* makedir(void *data);
-void* move(void *data);
-void* thread(void *arg);
+  char namafile[30]; 
+  char namafilebaru[30]; 
+  char ekstensi[10]; 
+  char dirawal[20]; 
+  char dirakhir[20];
+  char f[] = "-f"; 
+  char bintang = "*"; 
+  char d[] = "-d";
+  void* makedir(void *data);  
+  void* move(void *data); 
+  void* thread(void *arg);      
 
 if (-f)
 {
