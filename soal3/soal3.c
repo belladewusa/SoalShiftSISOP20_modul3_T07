d2877a20cb3147f2e76232ed213cd2b8ab89cf91
belladewusa <55181780+belladewusa@users.noreply.github.com>
2020-04-03 16:21:12 +0700

    Create soal3.c

---
+++ soal3/soal3.c
@@ -0,0 +1,9 @@
+#include<sys/types.h>
+#include<sys/wait.h>
+#include <sys/stat.h> 
+#include<stdio.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+#define MAX 256
